52a59575225b8f5c002fe6ca6b057558fbdd5eb8
Volker Gronau <vjay@gmx.net>
2016-10-13 20:54:45 +0200

    Server can now control client keyboard+mouse

--- src/de/volkerGronau/distributedClassroom/ScreenTransfer.java
+++ src/de/volkerGronau/distributedClassroom/ScreenTransfer.java
@@ -1,58 +1,66 @@
 package de.volkerGronau.distributedClassroom;
 
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.MouseInfo;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
 import javax.imageio.ImageIO;
 
-import de.volkerGronau.ApplicationHelper;
+import org.apache.commons.lang3.mutable.MutableBoolean;
+
 import de.volkerGronau.distributedClassroom.ProxyHelper.ProxyType;
+import javafx.scene.input.KeyCode;
 
 public class ScreenTransfer {
 
 	protected final Robot robot = new Robot();
-	protected Image cursorImage;
+
 	protected BufferedImage bufferedImage;
 	protected BufferedImage differenceImage;
+	protected BufferedImage imageToSendToServer;
 	protected Screen screen;
-	protected String urlString;
+	protected String urlBaseString;
 	protected Proxy proxy;
-	protected int interval = 1000;
+	protected long takeNextPictureAt = 0;
+	protected java.awt.Point cursorPosition;
+	protected int changedPixels;
+	protected boolean isInputControlledByServer;
+	protected long nextForcedContact = 0;
 
 	public ScreenTransfer(Screen screen, String name, String serverAddress) throws Exception {
 		super();
 
 		this.screen = screen;
-		this.urlString = serverAddress + "?userName=" + name;
+		this.urlBaseString = serverAddress + "?userName=" + name;
 
-		cursorImage = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
-		Graphics graphics = cursorImage.getGraphics();
-		graphics.drawImage(ApplicationHelper.Resources.getImage("cursorRed.png", false).getImage(), 0, 0, null);
-		graphics.dispose();
-
-		Thread thread = new Thread("Screen Grab Thread") {
+		Thread thread = new Thread("Server Interactionthread") {
 			@Override
 			public void run() {
 				try {
 					while (!isInterrupted()) {
+						//						long startTime = System.currentTimeMillis();
 						if (proxy == null) {
-							proxy = ProxyHelper.getProxy(urlString, ProxyType.os);
+							proxy = ProxyHelper.getProxy(urlBaseString, ProxyType.os);
 						}
-						grabScreen();
-						Thread.sleep(interval);
+						updateData();
+						//						long waitingTime = 100l - (System.currentTimeMillis() - startTime);
+						//						if (waitingTime > 0) {
+						//							Thread.sleep(waitingTime);
+						//						}
 					}
-				} catch (InterruptedException e) {
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
 		};
@@ -61,78 +69,189 @@ public class ScreenTransfer {
 		thread.start();
 	}
 
-	public void grabScreen() {
+	public void updateData() {
 		try {
-			BufferedImage newBufferedImage = robot.createScreenCapture(screen.getBounds());
-			if (differenceImage == null) {
-				differenceImage = new BufferedImage(newBufferedImage.getWidth(), newBufferedImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+			long currentTimeMillis = System.currentTimeMillis();
+			boolean callSendDataToServer = false;
+
+			if (takeNextPictureAt < currentTimeMillis) {
+				BufferedImage newBufferedImage = robot.createScreenCapture(screen.getBounds());
+				if (differenceImage == null) {
+					differenceImage = new BufferedImage(newBufferedImage.getWidth(), newBufferedImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+				}
+
+				if (bufferedImage == null) {
+					imageToSendToServer = newBufferedImage;
+					bufferedImage = newBufferedImage;
+					callSendDataToServer = true;
+					changedPixels = 0;
+				} else {
+					changedPixels = getChangedPixels(bufferedImage, newBufferedImage, differenceImage);
+					if (changedPixels > 20) {
+						imageToSendToServer = differenceImage;
+						bufferedImage = newBufferedImage;
+						callSendDataToServer = true;
+					} else {
+						imageToSendToServer = null;
+					}
+				}
+			} else {
+				imageToSendToServer = null;
 			}
 
-			//TODO: transfer cursor position to the server and let it paint it on top of the image -> saves bandwith and much smoother animation
-			java.awt.Point point = MouseInfo.getPointerInfo().getLocation();
-			point.x -= screen.getBounds().x;
-			point.y -= screen.getBounds().y;
-			Graphics graphics = newBufferedImage.getGraphics();
-			graphics.drawImage(cursorImage, point.x, point.y, null);
-			graphics.dispose();
-
-			if (bufferedImage == null || isImageDifferent(bufferedImage, newBufferedImage, differenceImage)) {
-				//				ImageIO.write(bufferedImage, "PNG", new File("c:\\temp\\test.png"));
-				//				ImageIO.write(differenceImage, "PNG", new File("c:\\temp\\differenceImage.png"));
-
-				sendImage(bufferedImage == null ? newBufferedImage : differenceImage);
-				bufferedImage = newBufferedImage;
-
+			java.awt.Point newCursorPosition = MouseInfo.getPointerInfo().getLocation();
+			if (!newCursorPosition.equals(cursorPosition)) {
+				cursorPosition = newCursorPosition;
+				callSendDataToServer = true;
 			}
+
+			if (callSendDataToServer || isInputControlledByServer || nextForcedContact < currentTimeMillis) {
+				sendDataToServer();
+				nextForcedContact = currentTimeMillis + 3000;
+			}
+
 		} catch (Exception e) {
 			bufferedImage = null;
 			e.printStackTrace();
 		}
 	}
 
-	protected void sendImage(BufferedImage bufferedImage) throws Exception {
-		URL url = new URL(urlString);
-		URLConnection conn = url.openConnection(proxy);
-		conn.setDoOutput(true);
-		((HttpURLConnection) conn).setRequestMethod("POST");
+	protected void sendDataToServer() throws Exception {
+		MutableBoolean result = new MutableBoolean(false);
 
-		boolean ok = false;
-		try (OutputStream os = conn.getOutputStream()) {
-			ImageIO.write(bufferedImage, "PNG", os);
-			os.flush();
+		Thread sendThread = new Thread() {
 
-			if (((HttpURLConnection) conn).getResponseCode() == 200) {
-				try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-					ok = "OK".equals(reader.readLine());
-					interval = Integer.parseInt(reader.readLine());
+			@Override
+			public void run() {
+				try {
+					StringBuilder serverURL = new StringBuilder(urlBaseString);
+					if (imageToSendToServer != null) {
+						serverURL.append("&imageIsUpdate=").append(imageToSendToServer == differenceImage);
+						serverURL.append("&changedPixels=").append(changedPixels);
+					}
+					if (screen.getBounds().contains(cursorPosition)) {
+						serverURL.append("&cursorX=").append(cursorPosition.x - screen.getBounds().x);
+						serverURL.append("&cursorY=").append(cursorPosition.y - screen.getBounds().y);
+					}
+					if (proxy != null) {
+						serverURL.append("&r=").append(Math.random()); // Lots of proxies cache anyway, we trick it
+					}
+
+					URL url = new URL(serverURL.toString());
+					URLConnection connection = url.openConnection(proxy);
+					connection.setUseCaches(false);
+					connection.setConnectTimeout(2000);
+					connection.setReadTimeout(5000);
+					if (imageToSendToServer != null) {
+						connection.setDoOutput(true);
+						((HttpURLConnection) connection).setRequestMethod("POST");
+						try (OutputStream os = connection.getOutputStream()) {
+							ImageIO.write(imageToSendToServer, "PNG", os);
+							os.flush();
+						}
+					}
+
+					if (((HttpURLConnection) connection).getResponseCode() == 200) {
+						try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+							try {
+								result.setValue("OK".equals(reader.readLine()));
+								isInputControlledByServer = Boolean.parseBoolean(reader.readLine());
+								if (result.isTrue()) {
+									processServerResponse(reader);
+								}
+							} catch (Exception e) {
+								while (reader.read() != -1) {
+								}
+								((HttpURLConnection) connection).disconnect();
+							}
+						}
+					}
+				} catch (Exception e) {
+					proxy = null; // Reset detected proxy on communication error
+					e.printStackTrace();
+					result.setFalse();
 				}
 			}
+
+		};
+		sendThread.setDaemon(false);
+		sendThread.start();
+		sendThread.join(10000);
+		if (sendThread.isAlive()) {
+			sendThread.interrupt();
+			throw new Exception("Timeout occured.");
 		}
 
-		if (!ok) {
+		if (result.isFalse()) {
 			throw new Exception("Did not get expected answer from server.");
 		}
 
 	}
 
-	protected boolean isImageDifferent(BufferedImage oldImage, BufferedImage newImage, BufferedImage differenceImage) {
+	protected String readNextToken(BufferedReader reader) throws IOException {
+		//		StringBuilder result = new StringBuilder();
+		//		char[] c = new char[1];
+		//		while (reader.read(c) != -1) {
+		//			if (c[0] == '\0') {
+		//				return result.toString();
+		//			}
+		//			result.append(c[0]);
+		//		}
+		//		return null;
+		return reader.readLine();
+	}
+
+	protected void processServerResponse(BufferedReader reader) throws NumberFormatException, IOException {
+		int pictureInterval = Integer.parseInt(reader.readLine());
+		if (imageToSendToServer != null) {
+			takeNextPictureAt = System.currentTimeMillis() + pictureInterval;
+		}
+		String command;
+		while ((command = readNextToken(reader)) != null) {
+			switch (command) {
+				case "kp" :
+					robot.keyPress(getCodeForKey(readNextToken(reader)));
+					break;
+				case "kr" :
+					robot.keyRelease(getCodeForKey(readNextToken(reader)));
+					break;
+				case "mm" :
+					robot.mouseMove(screen.getBounds().x + Integer.parseInt(readNextToken(reader)), screen.getBounds().y + Integer.parseInt(readNextToken(reader)));
+					break;
+				case "mp" :
+					robot.mousePress(InputEvent.getMaskForButton(Integer.parseInt(readNextToken(reader))));
+					break;
+				case "mr" :
+					robot.mouseRelease(InputEvent.getMaskForButton(Integer.parseInt(readNextToken(reader))));
+					break;
+			}
+
+		}
+	}
+
+	protected int getCodeForKey(String key) {
+		return KeyCode.valueOf(key).impl_getCode();
+		//return KeyEvent.getKeyCodeForChar(key.charAt(0));
+	}
+
+	protected int getChangedPixels(BufferedImage oldImage, BufferedImage newImage, BufferedImage differenceImage) {
 		int[] pixelsOld = ((DataBufferInt) oldImage.getRaster().getDataBuffer()).getData();
 		int[] pixelsNew = ((DataBufferInt) newImage.getRaster().getDataBuffer()).getData();
 		int[] pixelsDifference = ((DataBufferInt) differenceImage.getRaster().getDataBuffer()).getData();
 
-		boolean diff = false;
+		int result = 0;
 		for (int index = pixelsOld.length - 1; index >= 0; index--) {
 			if (pixelsOld[index] == pixelsNew[index]) {
-				// equal, we can set the difference to transparent
+				// equal, we set the difference to transparent
 				pixelsDifference[index] = 0;
 			} else {
-				// all equal, we can set the difference to the difference
+				// not equal, we set the difference to the difference
 				pixelsDifference[index] = pixelsNew[index] + 0xFF000000;
-				diff = true;
+				result++;
 			}
 		}
 
-		return diff;
+		return result;
 	}
 
 }
--- src/de/volkerGronau/distributedClassroom/serverWindow/ServerWindowController.java
+++ src/de/volkerGronau/distributedClassroom/serverWindow/ServerWindowController.java
@@ -1,32 +1,44 @@
 package de.volkerGronau.distributedClassroom.serverWindow;
 
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
 
 import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import de.volkerGronau.ApplicationHelper;
 import de.volkerGronau.distributedClassroom.settings.Settings;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import sun.awt.image.IntegerComponentRaster;
 
 public class ServerWindowController implements HttpHandler {
 
@@ -36,10 +48,23 @@ public class ServerWindowController implements HttpHandler {
 	@FXML
 	protected FlowPane flowPane;
 
-	protected Map<String, BufferedImage> imageCache = Maps.newHashMap();
-	protected Map<String, BorderPane> guiElementCache = Maps.newHashMap();
+	protected static class Client {
+		String userName;
+		BufferedImage bufferedImage;
+		WritableImage fxImage;
+		java.awt.Point cursorPos;
+		BorderPane borderPane; // root element for user
+		ImageView imageView; // his/her image
+		List<String> commands = Lists.newArrayList();
+	}
+
+	protected Map<String, Client> clientCache = Maps.newHashMap();
+	protected int[] cursorImageData;
+	protected int cursorImageOffset;
+	protected int cursorImageScan;
 
 	protected String openedUserName;
+	protected boolean isControlling;
 
 	public void init(Stage stage, ResourceBundle resources, Settings settings) throws Exception {
 		stage.setTitle(resources.getString("title"));
@@ -47,6 +72,15 @@ public class ServerWindowController implements HttpHandler {
 		scrollPane.viewportBoundsProperty().addListener((obs, old, bounds) -> {
 			flowPane.setPrefWidth(bounds.getMaxX() - bounds.getMinX());
 		});
+
+		BufferedImage cursorBufferedImage = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+		Graphics graphics = cursorBufferedImage.getGraphics();
+		graphics.drawImage(ApplicationHelper.Resources.getImage("cursorRed.png", false).getImage(), 0, 0, null);
+		graphics.dispose();
+		IntegerComponentRaster icr = (IntegerComponentRaster) cursorBufferedImage.getRaster();
+		cursorImageData = icr.getDataStorage();
+		cursorImageOffset = icr.getDataOffset(0);
+		cursorImageScan = icr.getScanlineStride();
 
 		HttpServer server = HttpServer.create(new InetSocketAddress(settings.getServerPort()), 0);
 		server.createContext("/DistributedClassroom", this);
@@ -67,6 +101,8 @@ public class ServerWindowController implements HttpHandler {
 
 	@Override
 	public void handle(HttpExchange httpExchange) throws IOException {
+		String result = "OK";
+
 		Map<String, String> parameters = Maps.newHashMap();
 		for (String param : Splitter.on("&").split(httpExchange.getRequestURI().getQuery())) {
 			int index = param.indexOf('=');
@@ -76,68 +112,180 @@ public class ServerWindowController implements HttpHandler {
 		}
 
 		String userName = parameters.get("userName");
-		BufferedImage bufferedImage = ImageIO.read(httpExchange.getRequestBody());
 
-		synchronized (imageCache) {
-			BufferedImage previousImage = imageCache.get(userName);
-			if (previousImage == null) {
-				synchronized (bufferedImage) {
-					imageCache.put(userName, bufferedImage);
-					notifyOfNewImage(userName, bufferedImage);
-				}
-
-			} else {
-				synchronized (previousImage) {
-					Graphics g = previousImage.getGraphics();
-					g.drawImage(bufferedImage, 0, 0, null);
-					g.dispose();
-					notifyOfNewImage(userName, previousImage);
-				}
+		Client client;
+		synchronized (clientCache) {
+			client = clientCache.get(userName);
+			if (client == null) {
+				client = new Client();
+				client.userName = userName;
+				clientCache.put(userName, client);
 			}
 		}
 
-		String response = "OK\n" + (userName.equals(openedUserName) ? "100" : "5000");
+		if (parameters.containsKey("cursorX")) {
+			client.cursorPos = new Point(Integer.parseInt(parameters.get("cursorX")), Integer.parseInt(parameters.get("cursorY")));
+		} else {
+			client.cursorPos = null;
+		}
+		System.out.println(httpExchange.getRequestURI().getQuery());
+		if (parameters.containsKey("imageIsUpdate")) {
+			BufferedImage transferedImage = ImageIO.read(httpExchange.getRequestBody());
+			if (client.bufferedImage != null) {
+				Graphics g = client.bufferedImage.getGraphics();
+				g.drawImage(transferedImage, 0, 0, null);
+				g.dispose();
+			} else {
+				if (Boolean.parseBoolean(parameters.get("imageIsUpdate"))) { // we got an update but we have nothing to update
+					result = "NOK, got an update-image but have no base";
+				} else {
+					client.bufferedImage = transferedImage;
+				}
+			}
+		}
+		//		System.out.println("result:" + result);
+		if ("OK".equals(result)) {
+			notifyOfNewImage(userName, client);
+		}
+
+		StringBuilder response = new StringBuilder(result).append('\n').append(userName.equals(openedUserName) && isControlling).append('\n');
+		response.append(userName.equals(openedUserName) ? "0" : "5000").append('\n'); // if opened user update the picture fast, otherwise slowly
+		synchronized (client.commands) {
+			for (String line : client.commands) {
+				response.append(line);
+			}
+			client.commands.clear();
+		}
+
 		httpExchange.sendResponseHeaders(200, response.length());
 		OutputStream os = httpExchange.getResponseBody();
-		os.write(response.getBytes());
+		os.write(response.toString().getBytes(StandardCharsets.UTF_8));
 		os.close();
 	}
 
-	protected void notifyOfNewImage(String userName, BufferedImage bufferedImage) {
-		Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+	protected void notifyOfNewImage(String userName, Client client) {
+		client.fxImage = SwingFXUtils.toFXImage(client.bufferedImage, client.fxImage);
+		if (client.cursorPos != null) {
+			try {
+				int maxHeight = client.cursorPos.y + Math.min(32, client.bufferedImage.getHeight() - client.cursorPos.y);
+				int maxWidth = client.cursorPos.x + Math.min(32, client.bufferedImage.getWidth() - client.cursorPos.x);
+				//
+				//				client.fxImage.getPixelWriter().setPixels(client.cursorPos.x, client.cursorPos.y, maxWidth, maxHeight, PixelFormat.getIntArgbInstance(), cursorImageData, cursorImageOffset, cursorImageScan);
+
+				int index = 0;
+				int color;
+				PixelWriter pixelWriter = client.fxImage.getPixelWriter();
+				for (int x = client.cursorPos.x; x < maxWidth; x++) {
+					for (int y = client.cursorPos.y; y < maxHeight; y++) {
+						color = cursorImageData[index++];
+						if (color >> 24 != 0) {
+							pixelWriter.setArgb(x, y, color);
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		Platform.runLater(() -> {
-			updateGUI(userName, image);
+			updateGUI(client);
 		});
 	}
 
-	protected void updateGUI(String userName, Image image) {
-		BorderPane borderPane = guiElementCache.get(userName);
-		if (borderPane == null) {
+	protected void updateGUI(Client client) {
+		if (client.borderPane == null) {
 			ImageView imageView = new ImageView();
 			imageView.setFitWidth(400);
 			imageView.setFitHeight(400);
 			imageView.setPreserveRatio(true);
 			imageView.setOnMouseClicked(e -> {
-				ImageView iv = new ImageView();
-				iv.setImage(((ImageView) guiElementCache.get(userName).getCenter()).getImage());
-				scrollPane.setContent(iv);
-				openedUserName = userName;
-				iv.setOnMouseClicked(e2 -> {
-					scrollPane.setContent(flowPane);
-					openedUserName = null;
-				});
+				switchToSingleUserView(client);
 			});
-			borderPane = new BorderPane(imageView);
-			borderPane.setTop(new Label(userName));
-			flowPane.getChildren().add(borderPane);
-			guiElementCache.put(userName, borderPane);
+			client.borderPane = new BorderPane(imageView);
+			client.borderPane.setTop(new Label(client.userName));
+			flowPane.getChildren().add(client.borderPane);
 		}
 
-		((ImageView) borderPane.getCenter()).setImage(image);
-
-		if (userName.equals(openedUserName)) {
-			((ImageView) scrollPane.getContent()).setImage(image);
+		if (openedUserName == null) {
+			((ImageView) client.borderPane.getCenter()).setImage(client.fxImage);
+		} else if (client.userName.equals(openedUserName)) {
+			((ImageView) ((StackPane) scrollPane.getContent()).getChildren().get(0)).setImage(client.fxImage);
 		}
+	}
+
+	private void switchToSingleUserView(Client client) {
+
+		Button backButton = new Button("Back");
+		backButton.setOnAction(e -> {
+			scrollPane.setContent(flowPane);
+			openedUserName = null;
+			isControlling = false;
+		});
+		CheckBox takeControlCheckBox = new CheckBox("Take Control");
+		takeControlCheckBox.selectedProperty().addListener((obs, old, selected) -> {
+			isControlling = selected;
+		});
+
+		ImageView imageView = new ImageView();
+		imageView.setImage(client.fxImage);
+		imageView.setFocusTraversable(true);
+
+		HBox hBox = new HBox(backButton, takeControlCheckBox);
+
+		StackPane stackPane = new StackPane(imageView, hBox);
+		stackPane.setAlignment(Pos.TOP_LEFT);
+		stackPane.setFocusTraversable(true);
+
+		imageView.setOnKeyPressed(e -> {
+			if (takeControlCheckBox.isSelected()) {
+				synchronized (client.commands) {
+					client.commands.add("kp\n" + e.getCode().name() + "\n");
+				}
+			}
+		});
+		imageView.setOnKeyReleased(e -> {
+			if (takeControlCheckBox.isSelected()) {
+				synchronized (client.commands) {
+					client.commands.add("kr\n" + e.getCode().name() + "\n");
+				}
+			}
+		});
+		stackPane.setOnMouseMoved(e -> {
+			if (takeControlCheckBox.isSelected()) {
+				synchronized (client.commands) {
+					client.commands.add("mm\n" + ((int) e.getX()) + "\n" + ((int) e.getY()) + "\n");
+				}
+			}
+		});
+		stackPane.setOnMouseDragged(e -> {
+			if (takeControlCheckBox.isSelected()) {
+				synchronized (client.commands) {
+					client.commands.add("mm\n" + ((int) e.getX()) + "\n" + ((int) e.getY()) + "\n");
+				}
+			}
+		});
+		stackPane.setOnMousePressed(e -> {
+			if (takeControlCheckBox.isSelected()) {
+				synchronized (client.commands) {
+					client.commands.add("mp\n" + e.getButton().ordinal() + "\n");
+				}
+			}
+		});
+		stackPane.setOnMouseReleased(e -> {
+			if (takeControlCheckBox.isSelected()) {
+				synchronized (client.commands) {
+					client.commands.add("mr\n" + e.getButton().ordinal() + "\n");
+				}
+			}
+		});
+		imageView.focusedProperty().addListener((obs, old, focused) -> {
+			imageView.requestFocus();
+		});
+
+		scrollPane.setContent(stackPane);
+		openedUserName = client.userName;
+		imageView.requestFocus();
+
 	}
 
 }
